d4b35e6addd321bacc7adade9731996cae998360
Sella08 <szahfitri@gmail.com>
2025-06-10 14:43:32 +0700

    Add files via upload

---
+++ scriptkipas.js
@@ -0,0 +1,7 @@
+function kipasOn() {
+  document.getElementById('baling-group').classList.add('berputar');
+}
+function kipasOff() {
+  document.getElementById('baling-group').classList.remove('berputar');
+}
+// Kipas tidak berputar saat awal, ON/OFF pakai tombol
---
+++ scriptlampu.js
@@ -0,0 +1,6 @@
+function lampuOn() {
+  document.getElementById('lampu-svg').classList.add('nyala');
+}
+function lampuOff() {
+  document.getElementById('lampu-svg').classList.remove('nyala');
+}
